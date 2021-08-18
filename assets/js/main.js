b6bc5cd0bba7badffc0a125e0e49ef2e841a9c02
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-18 13:23:48 +0630

    #add: header js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,44 @@
-/*=============== SHOW MENU ===============*/
+// variables
+const domSelectors = {
+    navMenu: "nav-menu",
+    navClose: "nav-close",
+    navToggle: "nav-toggle",
+    navLink: ".nav__link",
+};
 
+const classes = {
+    showMenu: "show-menu",
+};
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById(domSelectors.navMenu),
+    navClose = document.getElementById(domSelectors.navClose),
+    navToggle = document.getElementById(domSelectors.navToggle);
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add(classes.showMenu);
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove(classes.showMenu);
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(domSelectors.navLink);
 
+function hideMenu() {
+    navMenu.classList.remove(classes.showMenu);
+}
+navLinks.forEach((link) => link.addEventListener("click", hideMenu));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
+/*==================== SHOW SCROLL UP ====================*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
